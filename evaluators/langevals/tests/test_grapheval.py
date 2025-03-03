372873270f1fe1c736791b90019bc9500e98f170
Eugenumber1 <y.budnyk789@gmail.com>
2025-03-03 22:01:31 +0100

    Made a working version of GraphEval

--- evaluators/langevals/tests/test_grapheval.py
+++ evaluators/langevals/tests/test_grapheval.py
@@ -3,6 +3,7 @@ from langevals_langevals.grapheval import (
     GraphEvalEntry,
     GraphEvalSettings,
 )
+from litellm.types.utils import ModelResponse
 
 
 def test_grapheval_evaluator_passed():
@@ -16,6 +17,8 @@ def test_grapheval_evaluator_passed():
     assert result.status == "processed"
     assert result.passed == True
 
+
+def test_grapheval_empty():
     entry = GraphEvalEntry(
         output="",
         contexts=[""],
@@ -26,18 +29,36 @@ def test_grapheval_evaluator_passed():
     assert result.status == "processed"
     assert result.passed == False
 
+
+def test_grapheval_2_contexts():
+    evaluator = GraphEvalEvaluator()
     result = evaluator.evaluate(
         GraphEvalEntry(
             output="The capital of France is Paris.",
-            contexts=["France is a country in Europe.", "Paris is a city in France."],
+            contexts=[
+                "France is a country in Europe.",
+                "Paris is a city in France.",
+                "Paris is the capital of France",
+            ],
         )
     )
-
     assert result.status == "processed"
     assert result.passed == True
 
 
 def test_grapheval_evaluator_failed():
+    entry = GraphEvalEntry(
+        output="John's effort is really appreciated!",
+        contexts=["Frank, your effor is really appreciated"],
+    )
+    evaluator = GraphEvalEvaluator(settings=GraphEvalSettings())
+    result = evaluator.evaluate(entry)
+
+    assert result.status == "processed"
+    assert result.passed == False
+
+
+def test_graphevla_i_dont_know():
     entry = GraphEvalEntry(
         output="I don't know the answer, please try again later.",
         contexts=[
@@ -50,16 +71,7 @@ def test_grapheval_evaluator_failed():
 
     assert result.status == "processed"
     assert result.passed == False
-
-    entry = GraphEvalEntry(
-        output="John's effort is really appreciated!",
-        contexts=["Frank, your effor is really appreciated"],
-    )
-    evaluator = GraphEvalEvaluator(settings=GraphEvalSettings())
-    result = evaluator.evaluate(entry)
-
-    assert result.status == "processed"
-    assert result.passed == False
+    assert result.cost
 
 
 def test_grapheval_knowledge_graph_construction_output():
@@ -70,7 +82,18 @@ def test_grapheval_knowledge_graph_construction_output():
     evaluator = GraphEvalEvaluator(settings=GraphEvalSettings())
     result = evaluator._construct_knowledge_graph(entry.output)
     print(result)
-    assert type(result) == list
-    assert result == [
+    assert isinstance(result, ModelResponse)
+    assert evaluator._get_arguments(result, value="triples") == [
         ["Italy", "had 3.6x times more cases of coronavirus than", "China"]
     ]
+
+
+def test_grapheval_cost():
+    entry = GraphEvalEntry(
+        output="Italy had 3.6x times more cases of coronavirus than China",
+        contexts=["John's efforts are really appreciated"],
+    )
+    evaluator = GraphEvalEvaluator(settings=GraphEvalSettings())
+    result = evaluator.evaluate(entry)
+    assert result.status == "processed"
+    assert result.cost and result.cost.amount > 0.0001
